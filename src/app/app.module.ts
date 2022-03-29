222001dc14157f7b8b562932b9f4c1ac4d63df3b
godzyken <isgodzy@gmail.com>
2022-03-29 16:11:54 +0200

    course Single Page Application Dialog
    Signed-off-by: godzyken <isgodzy@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
+import { MatDialogModule } from '@angular/material/dialog';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -20,6 +21,7 @@ import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { LeaderDetailsComponent } from './leader-details/leader-details.component';
+import { LoginComponent } from './login/login.component';
 
 import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
@@ -35,7 +37,8 @@ import { LeaderService } from './services/leader.service';
     FooterComponent,
     AboutComponent,
     ContactComponent,
-    LeaderDetailsComponent
+    LeaderDetailsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -47,12 +50,16 @@ import { LeaderService } from './services/leader.service';
     MatCardModule,
     MatButtonModule,
     MatInputModule,
+    MatDialogModule,
     BrowserAnimationsModule,
   ],
   providers: [
     DishService,
     PromotionService,
     LeaderService
+  ],
+  entryComponents: [
+    LoginComponent
   ],
   bootstrap: [AppComponent]
 })
