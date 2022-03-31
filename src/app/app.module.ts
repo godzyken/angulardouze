17d36f2edb7daa92daa0fee8156b820f8074cf9b
godzyken <isgodzy@gmail.com>
2022-03-31 14:03:15 +0200

    cours angular Directives
    Signed-off-by: godzyken <isgodzy@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -38,6 +38,7 @@ import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
 import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
@@ -50,7 +51,8 @@ import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
     AboutComponent,
     ContactComponent,
     LeaderDetailsComponent,
-    LoginComponent
+    LoginComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
